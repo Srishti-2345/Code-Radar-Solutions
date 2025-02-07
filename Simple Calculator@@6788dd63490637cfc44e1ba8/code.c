2a50153051ae391412c7e4794b966fd766977d0a
Srishti-2345 <168220010+Srishti-2345@users.noreply.github.com>
2025-02-07 19:14:22 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,44 +1,44 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    double num1, num2, result;
-    char operator;
+    int num1, num2;
+        char operator;
+            int result;
 
-    // Prompt user for input
- 
-    scanf("%lf", &num1);
-    scanf("%lf", &num2);
-    
-    scanf(" %c", &operator);
-    
-    
-    
+                if (scanf("%d %d %c", &num1, &num2, &operator) != 3) {
+                        fprintf(stderr, "Invalid input format.\n");
+                                return 1;
+                                    }
 
-    // Perform the calculation based on the operator
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            printf("Result: %.2lf\n", result);
-            break;
-        case '-':
-            result = num1 - num2;
-            printf("Result: %.2lf\n", result);
-            break;
-        case '*':
-            result = num1 * num2;
-            printf("Result: %.2lf\n", result);
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-                printf("Result: %.2lf\n", result);
-            } else {
-                printf("Error: Division by zero is not allowed.\n");
-            }
-            break;
-        default:
-            printf("Invalid operator! Please use +, -, *, or /.\n");
-    }
+                                        switch (operator) {
+                                                case '+':
+                                                            result = num1 + num2;
+                                                                        printf("%d\n", result);
+                                                                                    break;
+                                                                                            case '-':
+                                                                                                        result = num1 - num2;
+                                                                                                                    printf("%d\n", result);
+                                                                                                                                break;
+                                                                                                                                        case '*':
+                                                                                                                                                    result = num1 * num2;
+                                                                                                                                                                printf("%d\n", result);
+                                                                                                                                                                            break;
+                                                                                                                                                                                    case '/':
+                                                                                                                                                                                                if (num2 == 0) {
+                                                                                                                                                                                                                fprintf(stderr, "Error: Division by zero.\n");
+                                                                                                                                                                                                                                return 1;
+                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                        result = num1 / num2;
+                                                                                                                                                                                                                                                                    printf("%d\n", result);
+                                                                                                                                                                                                                                                                                break;
+                                                                                                                                                                                                                                                                                        default:
+                                                                                                                                                                                                                                                                                                    fprintf(stderr, "Error: Invalid operator.\n");
+                                                                                                                                                                                                                                                                                                                return 1;
+                                                                                                                                                                                                                                                                                                                    }
 
-    return 0;
-}
+                                                                                                                                                                                                                                                                                                                        return 0;
+                                                                                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                                                                                        
